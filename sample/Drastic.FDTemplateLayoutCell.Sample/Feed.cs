2ffc33df25dadbc5e987d86dc8b7d350beabe696
Timothy Miller <innerlogic4321@gmail.com>
2023-02-18 13:27:14 +0900

    Sample

---
+++ sample/Drastic.FDTemplateLayoutCell.Sample/Feed.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Drastic.FDTemplateLayoutCell.Sample
+{
+    public class Feed
+    {
+        [JsonPropertyName("username")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+
+        [JsonPropertyName("imageName")]
+        public string ImageName { get; set; }
+
+        [JsonPropertyName("time")]
+        public string Time { get; set; }
+
+    }
+
+    public class FeedModel
+    {
+        [JsonPropertyName("feed")]
+        public Feed[] Feed { get; set; }
+    }
+}
